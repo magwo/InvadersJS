124f927f8002bfa1750666a26d352391ab591dea
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2011-10-03 20:44:46 +0200

    Updated tests.

--- tests/mytests.js
+++ tests/mytests.js
@@ -1,7 +1,11 @@
 // Let's test this function  
-function isEven(val) {  
+function isEven(val) {
     return val % 2 === 0;  
-}  
+}
+
+function concatenate(s1, s2) {
+	return s1 + s2;
+}
   
 test('isEven()', function() { 
     ok(isEven(0), 'Zero is an even number'); 
@@ -10,3 +14,8 @@ test('isEven()', function() {
     ok(!isEven(1), 'One is not an even number'); 
     ok(!isEven(-7), 'Neither is negative seven');  
 })  
+
+
+test("shouldConcatenate", function() {
+	equals("string", concatenate("str", "ing"));
+})
